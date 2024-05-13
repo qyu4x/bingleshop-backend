a80b9542f407c4dc7aeda1391a03124a73b5791f
Ariq Khoiri <qq.khoiri@gmail.com>
2024-05-13 19:55:40 +0700

    feat: create auth middleware to check user token and role

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+const {ResponseError} = require('../error/response-error');
+const {User} = require('../model')
+
+const authorize = (hasRoles = []) => {
+    return async (req, res, next) => {
+        try {
+            const token = req.get('Authorization').trim();
+            if (!token) {
+                throw new ResponseError(401, "Unauthorized");
+            }
+
+            const user = await User.findOne({
+                where: {
+                    token: {
+                        [Op.eq]: token
+                    }
+                }
+            })
+
+            if (!user) {
+                throw new ResponseError(401, "Unauthorized");
+            }
+
+            const authorized = hasRoles.some(hasRole => hasRole.toUpperCase() === user.role.toUpperCase());
+            if (!authorized) {
+                throw new ResponseError(403, "Forbidden");
+            }
+
+            req.user = user;
+            next()
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = {
+    authorize
+}
