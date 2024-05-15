0bafbd1084a058f93e9ff5f371dc280d95cf007e
Ariq Khoiri <qq.khoiri@gmail.com>
2024-05-15 16:30:20 +0700

    fix: select order by created_at desc on address.list

--- service/address.service.js
+++ service/address.service.js
@@ -55,6 +55,9 @@ const list = async (userId) => {
         where: {
             user_id: userId, is_active: true
         },
+        order: [
+            ['created_at', 'ASC']
+        ],
         attributes: ['id', 'user_id', 'name', 'phone_number', 'street', 'province', 'city',
             'district', 'postal_code', 'is_main_address', 'is_active', 'created_at', 'updated_at']
     })
@@ -86,7 +89,7 @@ const remove = async (userId, addressId) => {
     addressId = validate(getAddressValidation, addressId);
     addressId = await checkAddressMustExist(userId, addressId);
 
-    const address = await Address.findOne( {
+    const address = await Address.findOne({
         where: {
             id: addressId, user_id: userId
         },
