d1f65b6a4a4f021128651f9678ed25cb6e8a14a0
Ariq Khoiri <qq.khoiri@gmail.com>
2024-06-04 19:09:09 +0700

    docs: setup swagger

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 const express = require('express');
-
 const path = require('path');
+
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const {errorMiddleware} = require("./middleware/error.middleware");
 const {routeNotFoundMiddleware} = require("./middleware/route-not-found.middleware");
 const {loggerMiddleware} = require("./middleware/logger.middleware");
 
+const YAML = require('yamljs');
+
 const {router} = require('./route/api');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/api/swagger.yaml'));
 
 const port = process.env.APP_PORT || 3000;
 const appName = process.env.APP_NAME || 'bingleshop';
 
 const app = express();
+
 app.use(loggerMiddleware);
 app.use(express.json());
 
 app.use("/", router);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorMiddleware);
 app.use(routeNotFoundMiddleware)
