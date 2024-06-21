8753092f6fc365182288fd1da85eecc306533d84
Djuliatama <dwina147@gmail.com>
2024-06-21 21:47:57 +0700

    add image validation in image.handler

--- src/handler/image.handler.js
+++ src/handler/image.handler.js
@@ -1,22 +1,44 @@
 const { uploadToImageKit } = require('../service/image.service');
+const storage = multer.memoryStorage();
 
-const uploadImage = async (req, res) => {
-    const file = req.file;
-    const product_id = req.product_id
-    const sequence = req.body.sequence;
-    const is_active = req.body.is_active || true;
-   
-    if (!file) {
-        return res.status(400).send('No file uploaded.');
-    }
+const upload = multer({
+        storage: storage, 
+        fileFilter: (req, file, cb) => {
+        const filetypes = /jpeg|jpg|png/;
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(file.originalname.split('.').pop());
+        if (mimetype && extname) {
+            return cb(null, true) 
+        }
+        cb(new Error('File type not supported (jpeg, jpg, png only)'));
+            
+        },
+        limits: {fileSize: 1000000 } //file maximum size 10MB
+    }).single('image');
 
-    try {
-        const response = await uploadToImageKit(file, product_id, sequence, is_active);
-        res.status(200).json({data:response});
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
+
+    const uploadImage = async (req, res) => {
+        upload(req, res, async (err) => {
+            if (err) {
+                return res.status(400).json({ error: err.message });
+            }
+            if (!req.file) {
+                return res.status(400).json({ error: 'No file uploaded' });
+            }
+            const file = req.file;
+            const product_id = req.body.product_id;
+            const sequence = req.body.sequence;
+            const is_active = req.body.is_active || true;
+    
+            try {
+                const response = await uploadToImageKit(file, product_id, sequence, is_active);
+                res.status(200).json({ data: response });
+            } catch (error) {
+                res.status(500).send(error);
+            }
+        });
+    };
+
 
 module.exports = { uploadImage };
 
