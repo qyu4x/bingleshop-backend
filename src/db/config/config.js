7177140b8f8fd4798707228c4fe29740f1a42fc2
arifnof <89186441+arifnof@users.noreply.github.com>
2024-07-18 20:24:32 +0700

    Update config.js

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -5,20 +5,20 @@ const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT, DB_PORT } =
 
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'secret',
-    database: 'bingleshop',
-    host: 'bingleshop-postgres',
-    dialect: 'postgres',
-    port: 5432,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    port: DB_PORT,
   },
   test: {
-    username: 'postgres',
-    password: 'secret',
-    database: 'bingleshop',
-    host: 'bingleshop-postgres',
-    dialect: 'postgres',
-    port: 5432,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    port: DB_PORT,
   },
   production: {
     username: DB_USERNAME,
